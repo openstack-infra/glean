b94841bbd9e91d9e21b09cfaf3d008ba587fd116
Gregory Haynes <greg@greghaynes.net>
2015-08-12 20:06:16 +0000

    Create systemwide lock for glean
    We support (and our init script does) running glean per-interface in
    parallel. We are also performing i/o on various files across the system
    during a run. We really need to only be doing this in one process at a
    time.
    
    Change-Id: I440e5ab067f5ef6161a2ab713d450fca9e4886a1

---
+++ glean/systemlock.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import fcntl
+
+
+class Lock(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        fh = open(self.filename, 'w')
+        fcntl.flock(fh, fcntl.LOCK_EX)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        fh = open(self.filename, 'w')
+        fcntl.flock(fh, fcntl.LOCK_UN)
